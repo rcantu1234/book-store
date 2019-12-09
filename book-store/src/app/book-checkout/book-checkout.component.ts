fa81ecd1e85bbe35f928732c93ef1a576d43a234
Yasuhiro Shimada <yshimada052@gmail.com>
2019-12-09 17:10:40 -0500

    roel

--- book-store/src/app/book-checkout/book-checkout.component.ts
+++ book-store/src/app/book-checkout/book-checkout.component.ts
@@ -3,40 +3,30 @@ import { BookComponent } from './../book/book.component';
 import { Component, OnInit, Input } from '@angular/core';
 import { Book } from '../models/book';
 import { HttpClient } from '@angular/common/http';
-
-
 @Component({
   selector: 'app-book-checkout',
   templateUrl: './book-checkout.component.html',
   styleUrls: ['./book-checkout.component.css']
 })
 export class BookCheckoutComponent implements OnInit {
-
   bookComponent: BookComponent;
-
   book: any;
-
   books: Book[] = [];
-
   // selectedBook: Book()
   selectedBook: Book;
-
   @Input() receivedTotal;
-
-  // @Input() public id: number;
-  // @Input() public author: string;
-  // @Input() public title: string;
-  // @Input() public price: number;
-
+  @Input() public id: number;
+  @Input() public avatar: string;
+  @Input() public author: string;
+  @Input() public title: string;
+  @Input() public price: number;
+  @Input() public url: string;
   constructor(private service: BookService) { }
-
   ngOnInit() {
+    this.onPurchaseBook(this.book);
   }
-
-  // @Input()
-  // onPurchaseBook(book: Book) {
-  //  return this.books.push(book);
-  // }
-
-
+  onPurchaseBook(book: Book) {
+    this.books.push(book);
+    console.log('TESTING!!!!');
+  }
 }
--- book-store/src/app/book/book.component.ts
+++ book-store/src/app/book/book.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BookService } from '../book.service';
 import { Book } from '../models/book';
-
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
@@ -10,28 +9,34 @@ import { Book } from '../models/book';
 })
 export class BookComponent implements OnInit {
   books: any;
-
   // total used for local
   total = 0;
-
   book: Book;
-
+  id: number;
+  avatar: string;
+  author: string;
+  title: string;
+  price: number;
+  url: string;
   selectedBook: Book;
-
   constructor(private service: BookService) {
   }
-
   ngOnInit() {
     this.service.getAllBooks().subscribe(data => this.books = data);
   }
-
   onSelectBook(book: Book): void {
     this.selectedBook = book;
   }
-
   onPurchaseBook(book: Book) {
     this.total = this.total + book.price;
-    console.log(this.total);
+    this.id = book.id;
+    this.avatar = book.avatar;
+    this.author = book.author;
+    this.title = book.title;
+    this.price = book.price;
+    this.url = book.url;
+    console.log(this.id + ' ' + this.avatar + ' ' + this.total + ' '
+    + this.id + ' ' + this.author + ' ' + this.title);
     // this.books.push(book);
   }
 }
